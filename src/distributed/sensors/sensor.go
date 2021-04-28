4b95b0d98c4eb6f890170f0f18428f5739ad5ff8
mohammed osama <mohammedosama@ieee.org>
2021-04-28 15:39:04 +0200

    register queue as util, dynamic queue and get queue,channel dynamically

--- src/distributed/sensors/sensor.go
+++ src/distributed/sensors/sensor.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+    "bytes"
+    "encoding/gob"
     "flag"
+    "go-building-queueing-distributed-system/src/distributed/dto"
+    "go-building-queueing-distributed-system/src/distributed/qutils"
     "log"
     "math/rand"
     "strconv"
     "time"
+
+    "github.com/streadway/amqp"
 )
 
 var name = flag.String("name", "sensor", "name of sensor")
@@ -16,14 +22,50 @@ var stepSize = flag.Float64("step", 0.1, "maximum allowable change per measureme
 var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 var value = r.Float64()*(*max-*min) + *min
 var nom = (*max-*min)/2 + *min
+var url = "amqp://guest:guest@localhost:5672"
 
 func main() {
     flag.Parse()
+    conn, ch := qutils.GetChannel(url)
+    defer conn.Close()
+    defer ch.Close()
+    dataQueue := qutils.GetQueue(*name, ch)
+    sensorQueue := qutils.GetQueue(qutils.SensorListQueue, ch)
+    msg := amqp.Publishing{
+        Body: []byte(*name),
+    }
+    // Publish the sensor queue name, to identify which queue we are currently at.
+    ch.Publish(
+        "", // direct methodolgy
+        sensorQueue.Name,
+        false,
+        false,
+        msg,
+    )
     dur, _ := time.ParseDuration(strconv.Itoa(1000/int(*freq)) + "ms")
     signal := time.Tick(dur)
-
+    // no need to register the buffer for each loop, but don't forget to reset at each loop.
+    buf := new(bytes.Buffer)
+    enc := gob.NewEncoder(buf)
     for range signal {
         calcValue()
+        reading := dto.SensorMessage{
+            Name:      *name,
+            Value:     value,
+            Timestamp: time.Now(),
+        }
+        buf.Reset()
+        enc.Encode(reading)
+        msg := amqp.Publishing{
+            Body: buf.Bytes(),
+        }
+        ch.Publish(
+            "", // direct methodolgy
+            dataQueue.Name,
+            false,
+            false,
+            msg,
+        )
         log.Printf("reading message. value: %v\n", value)
     }
 }
